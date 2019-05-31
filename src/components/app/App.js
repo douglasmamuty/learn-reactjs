6a901b19d37013af6c1d477b3fb49b4bce204b73
Douglas da Silva Saldanha <douglas.saldanha@amcom.com.br>
2019-05-31 18:37:41 -0300

    redux

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,42 +1,51 @@
-import React from 'react';
-import logo from '../../assets/images/logo.svg';
+import React, { Component } from 'react';
+// import logo from '../../assets/images/logo.svg';
 import './App.css';
-import {formatName} from '../../utils/Utils.js';
+// import {formatName} from '../../utils/Utils.js';
+import { connect } from 'react-redux';
 
 
 
-function App() {
-
-  const user = {
-    firstName: 'Douglas',
-    lastName: 'Saldanha'
-  };
+class App extends Component {
+  render(){
+    const { newValue } = this.props;
+  // const user = {
+  //   firstName: 'Douglas',
+  //   lastName: 'Saldanha'
+  // };
   
-  const getGreeting = (user) => {
-    if (user) {
-      return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
-  }
+  // const getGreeting = (user) => {
+  //   if (user) {
+  //     return <h1>Hello, {formatName(user)}!</h1>;
+  //   }
+  //   return <h1>Hello, Stranger.</h1>;
+  // }
   
-  const element = (
-    <div>
-      <h1>
-        Hello, {formatName(user)}!
-      </h1>    
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
+  // const element = (
+  //   <div>
+  //     <h1>
+  //       Hello, {formatName(user)}!
+  //     </h1>    
+  //     <h2>It is {new Date().toLocaleTimeString()}.</h2>
+  //   </div>
+  // );
 
   return (
-    <div>
-      <div>
-        {element}
+    <div className="App" style={{ paddingTop: '10px' }}>
+        <input type='text' />
+        <button>
+          Click me!
+        </button>
+        <h1>{newValue}</h1>
       </div>
-      <div>
-        {getGreeting()}      
-      </div>
-    </div>
+    // <div>
+    //   <div>
+    //     {element}
+    //   </div>
+    //   <div>
+    //     {getGreeting()}      
+    //   </div>
+    // </div>
     
     
     // <div className="App">
@@ -58,4 +67,8 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+
+export default connect(mapStateToProps)(App);
